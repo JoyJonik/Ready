0caceb6f49813ca23a39747c850b8ddd5a1b9cc1
JoyJonik <79088644+JoyJonik@users.noreply.github.com>
2021-02-21 20:41:11 +0500

    фцвфцв

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 5.2.4/15996
 // Filename: simplex_v5_autohide.ggsk
-// Generated Вс фев 21 20:29:53 2021
+// Generated Вс фев 21 20:40:55 2021
 
 function pano2vrSkin(player,base) {
 	var ggSkinVars = [];
